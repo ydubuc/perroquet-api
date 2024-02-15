efaafc8dede4811d1d9121fa99385e46d379a4ef
Yoan <yoan.dubuc@outlook.com>
2024-02-14 22:40:21 -0500

    Update access_info.rs

--- src/auth/models/access_info.rs
+++ src/auth/models/access_info.rs
@@ -3,6 +3,8 @@ use serde::Serialize;
 #[derive(Debug, Serialize)]
 pub struct AccessInfo {
     pub access_token: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub refresh_token: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub device_id: Option<String>,
 }
